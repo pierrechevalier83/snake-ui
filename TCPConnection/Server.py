da1b49e1b07f6fe18b407e8d6d4038d9b2beb371
Maria Viseu <mariav@localhost.localdomain>
2017-03-21 20:08:01 +0000

    Update TCP connection

--- TCPConnection/Server.py
+++ TCPConnection/Server.py
@@ -8,11 +8,11 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-print('Connection address:'), addr
-while l:
+print('Connection address:', addr)
+while True:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print("received data:"), data
+    print("received data:", data)
     conn.send(data)
 conn.close()
 
--- TCPConnection/client.py
+++ TCPConnection/client.py
@@ -2,16 +2,16 @@
 
 import socket
 
-TCP_IP = '192.168.20.73'
+TCP_IP = '127.0.0.1'
 #TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "42"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+s.send(MESSAGE.encode('utf-8'))
 data = s.recv(BUFFER_SIZE)
 s.close()
 
-print "received data:", data
+print("send data:",data)
