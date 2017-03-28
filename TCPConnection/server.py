322fafca18d3807908a2ab86ca50edabb3dffeee
Maria Viseu <mariaviseu@gmail.com>
2017-03-28 19:02:09 +0100

    Fixing loop bug in server

--- TCPConnection/server.py
+++ TCPConnection/server.py
@@ -6,11 +6,9 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-
-conn, addr = s.accept()
-print('Connection address:', addr)
 l = True
 while l is True:
+    conn, addr = s.accept()
     data = conn.recv(BUFFER_SIZE)
     if data: print("received data:", data)
 conn.close()
