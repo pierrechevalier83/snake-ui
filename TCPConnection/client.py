9244477b1896f846002a406191e96b94e1fd592c
Maria Viseu <mariaviseu@gmail.com>
2017-03-28 19:27:56 +0100

    very small start on json converting

--- TCPConnection/client.py
+++ TCPConnection/client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import json
 import socket
 
 TCP_IP = '127.0.0.1'
@@ -7,8 +7,9 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "42"
+MESSAGE_JSON = json.dumps([{"Value": "42"}])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE.encode('utf-8'))
+s.send(MESSAGE_JSON)
 s.close()
