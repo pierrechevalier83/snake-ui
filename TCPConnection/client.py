ac56e1b495aea6599a84a994c99665a4fbae9b44
Robinson Crusoe <notreallymyemail@fake.com>
2017-04-04 19:17:37 +0100

    Created fully functional JSON client and server

--- TCPConnection/client.py
+++ TCPConnection/client.py
@@ -2,14 +2,20 @@
 import json
 import socket
 
+# Connect to server adress and port
 TCP_IP = '127.0.0.1'
-#TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = "42"
-MESSAGE_JSON = json.dumps([{"Value": "42"}])
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE_JSON)
+
+
+# Message to send
+#message_json = json.dumps([{"Value":"42"}])
+message_json = json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4)
+
+
+# Send data to the server
+s.send(message_json.encode())
+
+# Close socket after use
 s.close()
--- TCPConnection/server.py
+++ TCPConnection/server.py
@@ -1,16 +1,28 @@
 import socket
+import json
 
+# Define to server adress and port
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024
+buffer_size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
+
+# Listen for information
 s.listen(1)
 l = True
+debug1= "No bugs hurray!"
 while l is True:
+
+#   Wait for somebody to connect
     conn, addr = s.accept()
-    data = conn.recv(BUFFER_SIZE)
-    if data: print("received data:", data)
+#   Retrreive the data
+    data = conn.recv(buffer_size)
+
+#   Decode and print the data
+    test = data.decode()
+    print(test)
+    l = False
+
+print(debug1)
 conn.close()
-
-
